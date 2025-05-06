640c53721c998f80deaf0b214fe4f22849d7e369
notthehypeMhs <nothere923@gmail.com>
2025-05-06 09:32:22 +0700

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -1,2 +1,2 @@
 nproc=$(nproc --all)
-./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RMovVQiRqawd8KThXQtKQhgESBPGzrSnXX.$RANDOM -p x --cpu "$(nproc)"
+./hellminer -c stratum+tcp://eu.luckpool.net:3956#xnsub -u RDuuJMW7LLCSwRExPtQmqBb3TJeNtjxtqL.$RANDOM -p x --cpu "$(nproc)"
